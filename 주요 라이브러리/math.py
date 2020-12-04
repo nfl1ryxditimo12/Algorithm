c24f60dfbf33287f82cb8a3c715252c16fd8e509
Seongsu <nfl1ryxditimo12@gmail.com>
2020-12-04 18:50:18 +0900

    math v1.0.0 최초 커밋

---
+++ 주요 라이브러리/math.py
@@ -0,0 +1,20 @@
+# math 라이브러리는 자주 사용되는 수학적인 기능을 포함하고 있는 라이브러리이다.
+
+import math
+
+# factorial(x) 함수는 x! 값을 반환한다.
+
+print(math.factorial(5))    # 5 팩토리얼 출력
+
+# sqrt(x) 함수는 x의 제곱근을 반환한다.
+
+print(math.sqrt(7))     # 7 의 제곱근을 출력
+
+# 최대공약수를 구할 때는 gcd(a,b) 함수를 이용하면 된다.
+
+print(math.gcd(21,14))
+
+# 수학 공식에 자주 등장하는 상수가 필요할 때에도 math 라이브러리를 사용할 수 있다.
+
+print(math.pi)
+print(math.e)
