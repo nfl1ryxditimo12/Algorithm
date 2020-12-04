5368fc8e7e8e1c4123f10a1ce778a97e695220bf
Seongsu <nfl1ryxditimo12@gmail.com>
2020-12-04 18:44:43 +0900

    collections v1.0.0 최초 커밋

--- 주요 라이브러리/collections.py
+++ 주요 라이브러리/collections.py
@@ -16,4 +16,4 @@ counter = Counter(['red','blue','red','green','blue','blue'])
 print(counter['blue'])      # 'blue' 가 등장한 횟수 출력
 print(counter['green'])     # 'green' 이 등장한 횟수 출력
 print(counter['red'])       # 'red' 가 등장한 횟수 출력
-print(dict(counter))        # 딕셔너리 자료형으로 변경
+print(dict(counter))        # 사전 자료형으로 변경
